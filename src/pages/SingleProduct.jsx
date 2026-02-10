7d717f5c8098ee837627d54f6c41ea0b03b30bd5
maxsadbek <masadanb@gmail.com>
2026-02-10 15:08:39 +0500

    add

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -8,14 +8,18 @@ export const loader = async ({ params }) => {
 };
 
 const SingleProduct = () => {
-    const [productColor, setProductColor] = useState(colors[0]);
   const product = useLoaderData();
+
   const { title, price, image, description, category, colors, company } =
     product.attributes;
-  const fomatedPrice = formatPrice(price);
+
+  const formattedPrice = formatPrice(price);
+
+  const [productColor, setProductColor] = useState(colors[0]);
 
   return (
     <div className="align-element my-12">
+      {/* breadcrumbs */}
       <div className="breadcrumbs">
         <ul>
           <li>
@@ -24,37 +28,47 @@ const SingleProduct = () => {
           <li>
             <Link to="/products">Products</Link>
           </li>
+          <li>{title}</li>
         </ul>
       </div>
+
+      {/* product */}
       <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+        {/* image */}
         <img
           src={image}
           alt={title}
           className="w-96 h-96 object-cover rounded-lg lg:w-full"
         />
+
+        {/* info */}
         <div>
           <h1 className="capitalize text-3xl font-bold">{title}</h1>
+
           <h3 className="text-xl text-neutral-content font-bold mt-2">
             {company}
           </h3>
-          <p className="mt-3 text-xl ">{formatPrice}</p>
+
+          <p className="mt-3 text-xl">{formattedPrice}</p>
+
           <p className="mt-6 leading-8">{description}</p>
+
+          {/* colors */}
           <div className="mt-4">
-            {colors.map((color) => {
-              return (
-                <button
-                  key={color}
-                  type="button"
-                  className={`badge w-6 h-6 mr-2 cursor-pointer ${
-                    color === productColor && "border-2 border-secondary"
-                  }`}
-                  style={{ backgroundColor: color }}
-                  onClick={() => setProductColor(color)}
-                >
-                  <span className="sr-only">{color}</span>
-                </button>
-              );
-            })}
+            <h4 className="font-medium mb-2">Colors</h4>
+            {colors.map((color) => (
+              <button
+                key={color}
+                type="button"
+                className={`badge w-6 h-6 mr-2 cursor-pointer ${
+                  color === productColor ? "border-2 border-secondary" : ""
+                }`}
+                style={{ backgroundColor: color }}
+                onClick={() => setProductColor(color)}
+              >
+                <span className="sr-only">{color}</span>
+              </button>
+            ))}
           </div>
         </div>
       </div>
