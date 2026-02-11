d9a0e4d6ee028f130828f33a27f5a9a9dcd89d21
maxsadbek <masadanb@gmail.com>
2026-02-11 10:28:09 +0500

    add

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -28,31 +28,24 @@ const SingleProduct = () => {
           <li>
             <Link to="/products">Products</Link>
           </li>
-          <li>{title}</li>
         </ul>
       </div>
 
       {/* product */}
       <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
-        {/* image */}
         <img
           src={image}
           alt={title}
           className="w-96 h-96 object-cover rounded-lg lg:w-full"
         />
-
-        {/* info */}
         <div>
           <h1 className="capitalize text-3xl font-bold">{title}</h1>
 
           <h3 className="text-xl text-neutral-content font-bold mt-2">
             {company}
           </h3>
-
           <p className="mt-3 text-xl">{formattedPrice}</p>
-
           <p className="mt-6 leading-8">{description}</p>
-
           {/* colors */}
           <div className="mt-4">
             <h4 className="font-medium mb-2">Colors</h4>
