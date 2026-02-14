38dc2adefcd72300489027fd3c3e2a70e0cc92ec
maxsadbek <masadanb@gmail.com>
2026-02-14 13:24:32 +0500

    add

--- src/components/ProductContainer.jsx
+++ src/components/ProductContainer.jsx
@@ -1,10 +1,44 @@
-import ProductGrid  from "../components/ProductGrid";
+import { useLoaderData } from "react-router-dom";
+import ProductGrid from "../components/ProductGrid";
+import ProductList from "./ProductList";
+import { BsGearFill, BsList } from "react-icons/bs";
+import { useState } from "react";
 
 const ProductContainer = () => {
+  const { meta } = useLoaderData();
+  const totalProduct = meta.pagination.total;
+  const [layout, setLayout] = useState('grid')
+
+  const setActiveStyle = (patter) =>{
+    return `text-xl btn btn-cicrle btn-sm ${patter === layout ? "btn-primary text-primary-content" : "btn-ghost text- based-content"}`
+  }
   return (
-    <div>
-      <ProductGrid />
-    </div>
+    <>
+      {/* Header */}
+      <div className="flex justify-between items-center mt-8 border-b border-base-300 pb-5">
+        <h4 className="font-medium text-md ">
+          {totalProduct} product{totalProduct > 1 && "s"}
+        </h4>
+        <div className="flex gap-2">
+          <button type="button" className={setActiveStyle('grid')} onClick={() => setLayout('grid')}>
+            <BsGearFill />
+          </button>
+          <button type="button" className={setActiveStyle('list')} onClick={() => setLayout('list')}>
+            <BsList />
+          </button>
+        </div>
+      </div>
+        <div>
+          {
+            totalProduct === 0 ?(
+              <h5 className="text-2xl mt-16">Sorry no products matched</h5>
+            )
+            :(
+              layout === 'grid' ? <ProductGrid /> : <ProductList />
+            )
+          }
+        </div>
+    </>
   );
 };
 
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,36 @@
-// import { useLoaderData, Link } from "react-router-dom"
-// import { formatPrice } from "../utils"
+import { useLoaderData, Link } from "react-router-dom";
+import { formatPrice } from "../utils";
 const ProductList = () => {
-  // const {products}
+  const { products } = useLoaderData();
+  console.log(products);
+
   return (
-    <div>
+    <div className="pt-12 grid gap-y-8">
+      {products.map((product) => {
+        const { title, image, price, company } = product.attributes;
+        const dollars = formatPrice(price);
 
+        return (
+          <Link
+            to={`/products/${product.id}`}
+            key={product.id}
+            className="p-8 rounded-lg flex flex-col sm:flex-row gap-y-8 flex-wrap bg-base-100 hover:shadow-2xl duration-200 group shadow-xl "
+          >
+            <img
+              src={image}
+              alt={title}
+              className="h-24 w-24 rounded-lg sm:h-32 object-cover group-hover:scale-105 transition duration-300 hover:scale-105"
+            />
+            <div className="ml-0 sm:ml-16 ">
+              <h3 className="capitalize font-medium text-lg">{title}</h3>
+              <h4 className="capitalize text-md text-neutral-content">{company}</h4>
+            </div>
+              <p className="font-medium ml-0 sm:ml-auto text-lg">{dollars}</p>
+          </Link>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default ProductList
+export default ProductList;
